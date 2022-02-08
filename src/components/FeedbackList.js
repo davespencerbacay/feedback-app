746a6fba9b7e4966017cc6c04cbaf5ac17d8ca17
Dave Spencer Bacay <spencerbacay@gmail.com>
2022-02-08 23:05:29 +0800

    adding a animation

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 
 const FeedbackList = ({ feedback, deleteFeedback }) => {
   if (!feedback || feedback.length === 0) {
@@ -8,19 +9,42 @@ const FeedbackList = ({ feedback, deleteFeedback }) => {
       <p style={{ textAlign: "center", marginTop: "2em" }}>No feedback yet.</p>
     );
   }
+
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem
-            key={item.id}
-            item={item}
-            deleteFeedback={() => deleteFeedback(item.id)}
-          />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                deleteFeedback={() => deleteFeedback(item.id)}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem
+  //           key={item.id}
+  //           item={item}
+  //           deleteFeedback={() => deleteFeedback(item.id)}
+  //         />
+  //       );
+  //     })}
+  //   </div>
+  // );
 };
 
 FeedbackList.propTypes = {
